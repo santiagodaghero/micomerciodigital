cb6f064af4518c0fa9fff20984371d21db72ad76
Santiago Daghero <santiagodaghero@users.noreply.github.com>
2026-07-14 12:00:24 -0300

    Fix: scroll al tope al navegar entre páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 import HomePage from './pages/HomePage'
 import ServiceDetail from './pages/ServiceDetail'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/servicio/:id" element={<ServiceDetail />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/servicio/:id" element={<ServiceDetail />} />
+      </Routes>
+    </>
   )
 }
